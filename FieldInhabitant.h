f04f35f17aa47d5b0d81383c9fd02faf18ed8ce3
TheBaconMen <150746782+TheBaconMen@users.noreply.github.com>
2023-11-27 16:53:09 -0500

    Add files via upload

---
+++ FieldInhabitant.h
@@ -0,0 +1,27 @@
+/*
+Author: James Lizotte
+Date: 11/27/2023
+Description: Header file for the FieldInhabitant superclass
+*/
+
+#ifndef FIELDINHABITANT_H
+#define FIELDINHABITANT_H
+
+#include<iostream>
+using namespace std;
+
+class FieldInhabitant //FieldInhabitant Superclass
+{
+private:
+    string symbol;
+
+public:
+    FieldInhabitant(string); // Constructor function
+    
+    string getSymbol(); //Getter function for the symbol of the inhabitant
+    void setSymbol(string); //Setter function for the symbol of the inhabitant
+    
+    virtual ~FieldInhabitant() = 0; //Must be defined in the subclass
+};
+
+#endif
