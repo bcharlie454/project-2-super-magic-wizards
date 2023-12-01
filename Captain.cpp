c3158afc74fbb2347ae5aeaa737e1bd3e89de037
Rakesh Gummapu <114535552+Sunny42-stevens@users.noreply.github.com>
2023-11-30 19:05:50 -0500

    Update Captain.cpp

--- Captain.cpp
+++ Captain.cpp
@@ -1,41 +1,27 @@
 /*
 Author: Rakesh Gummapu
 Date: 11/29/2023
-Description: Function file for the Creature subclass
+Description: Header file for the Captain subclass
 */
 
-#include "Creature.h"
+#include "Captain.h"
 
-// Constructor for the Creature class
-// @param symbol: The Creature's symbol, passed to the FieldInhabitant constructor
-// @param x: Initial X-coordinate of the Creature
-// @param y: Initial Y-coordinate of the Creature
-Creature::Creature(std::string symbol, int x, int y) : FieldInhabitant(symbol), x(x), y(y) {}
+// Constructor for the Captain class
+// Initializes the Captain at a given position with a symbol 'V'
+// @param x: Initial X-coordinate of the Captain
+// @param y: Initial Y-coordinate of the Captain
+Captain::Captain(int x, int y) : Creature("V", x, y) {}
 
-// Moves the Creature by a given delta in the x and y directions
-// @param dx: Change in the X-coordinate
-// @param dy: Change in the Y-coordinate
-void Creature::move(int dx, int dy) {
-    x += dx;
-    y += dy;
+// Adds a Veggie object to the Captain's collection
+// @param veggie: Pointer to the Veggie object being collected
+void Captain::collectVeggie(Veggie* veggie) 
+{
+    collectedVeggies.push_back(veggie);
 }
 
-// Getter function for the Creature's X-coordinate
-// @return: The X-coordinate of the Creature
-int Creature::getX() const { 
-    return x; 
-}
-
-// Getter function for the Creature's Y-coordinate
-// @return: The Y-coordinate of the Creature
-int Creature::getY() const { 
-    return y; 
-}
-
-// Sets the position of the Creature
-// @param newX: New X-coordinate for the Creature
-// @param newY: New Y-coordinate for the Creature
-void Creature::setPosition(int newX, int newY) { 
-    x = newX; 
-    y = newY; 
+// Getter function for the collection of veggies
+// @return: Constant reference to the vector of collected Veggie pointers
+const std::vector<Veggie*>& Captain::getCollectedVeggies() const 
+{
+    return collectedVeggies;
 }
