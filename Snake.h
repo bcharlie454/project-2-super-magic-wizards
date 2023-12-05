3dc9f1d2b203b42dfe02b1768f1ce57b36845136
TheBaconMen <150746782+TheBaconMen@users.noreply.github.com>
2023-12-04 19:23:59 -0500

    Add files via upload

---
+++ Snake.h
@@ -0,0 +1,16 @@
+/*
+Author: James Lizotte
+Date: 11/30/2023
+Description: Header file for the Snake subclass
+*/
+
+#include"Creature.h"
+
+class Snake : public Creature
+{
+private:
+    
+
+public:
+    Snake(int, int);
+};
